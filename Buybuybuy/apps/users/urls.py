9a624891eb9f4ee62f599f4f5b86350001cfd2ce
zlyuzhi <zl_8876@163.com>
2018-10-30 20:11:41 +0800

    使用缓存保存收货地址位置1

--- Buybuybuy/apps/users/urls.py
+++ Buybuybuy/apps/users/urls.py
@@ -4,6 +4,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 # eg:url(r'^book/$',views.BookView.as_view()),
+
 urlpatterns = [
     url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
